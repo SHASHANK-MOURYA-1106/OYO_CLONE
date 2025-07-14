ae8d89dab8e8c9133d871ca8d5d4e6b1018d6b0d
SHASHANK MOURYA PIPPARI <69301612+SHASHANK-MOURYA-1106@users.noreply.github.com>
2025-07-14 19:56:02 +0530

    Update settings.py

--- oyo_clone/settings.py
+++ oyo_clone/settings.py
@@ -90,12 +90,8 @@ USER = env.str("USER")
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': NAME,       # the name you gave in phpMyAdmin
-        'USER': USER,       # default XAMPP MySQL user
-        'PASSWORD': '',               # default XAMPP MySQL has no password for root
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
